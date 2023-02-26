c2306de653f26d0df208308a90946df1783c7cbb
Soumen Sikder Shuvo <soumen123898@gmail.com>
2023-02-26 16:41:58 -0500

    Update test1.cu

--- test1.cu
+++ test1.cu
@@ -1,40 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TILE_SIZE 16
+#define BLOCK_SIZE 32
 
 __global__ void matrixMul(float* A, float* B, float* C, int rowDimA, int colDimA, int colDimB) {
-    __shared__ float s_A[TILE_SIZE][TILE_SIZE];
-    __shared__ float s_B[TILE_SIZE][TILE_SIZE];
+    __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
 
-    int bx = blockIdx.x, by = blockIdx.y, tx = threadIdx.x, ty = threadIdx.y;
-    int row = by * TILE_SIZE + ty;
-    int col = bx * TILE_SIZE + tx;
+    // Block index
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
 
-    float cValue = 0;
+    // Thread index
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
 
-    for (int t = 0; t < (colDimA + TILE_SIZE - 1) / TILE_SIZE; t++) {
-        if (row < rowDimA && t * TILE_SIZE + tx < colDimA)
-            s_A[ty][tx] = A[row * colDimA + t * TILE_SIZE + tx];
+
+    int row = by * BLOCK_SIZE + ty;
+    int col = bx * BLOCK_SIZE + tx;
+
+    // Csub is used to store the element of the block sub-matrix that is computed by the thread
+    float Csub = 0;
+    
+//     To handle boundary conditions when dealing with arbitrary matrix sizes,
+//     we need to make sure that each thread only processes valid elements within
+//     the matrix dimensions. We can achieve this by adding conditional statements within
+//     the kernel to check whether the thread index is within the valid range.
+    
+    // We use conditional statements to check whether each thread is within the valid range 
+    // for the matrices A and B. If a thread index is outside the valid range, we set the 
+    // corresponding shared memory value to 0.0, which effectively ignores those elements 
+    // during the computation.
+    
+    // Note that we only need to add these conditional statements for the shared memory portion of 
+    // the computation, since the thread block size is fixed and we can assume that the total number 
+    // of threads is always a multiple of the block size. The boundary conditions for the global memory
+    // portion of the computation are already handled by the grid size and block size calculation in the host code
+
+    for (int t = 0; t < (colDimA + BLOCK_SIZE - 1) / BLOCK_SIZE; t++) {
+        if (row < rowDimA && t * BLOCK_SIZE + tx < colDimA)
+            As[ty][tx] = A[row * colDimA + t * BLOCK_SIZE + tx];
         else
-            s_A[ty][tx] = 0;
+            As[ty][tx] = 0;
 
-        if (t * TILE_SIZE + ty < colDimA && col < colDimB)
-            s_B[ty][tx] = B[(t * TILE_SIZE + ty) * colDimB + col];
+        if (t * BLOCK_SIZE + ty < colDimA && col < colDimB)
+            Bs[ty][tx] = B[(t * BLOCK_SIZE + ty) * colDimB + col];
         else
-            s_B[ty][tx] = 0;
+            Bs[ty][tx] = 0;
 
         __syncthreads();
+        
 
-        for (int i = 0; i < TILE_SIZE; i++) {
-            cValue += s_A[ty][i] * s_B[i][tx];
+        for (int k = 0; k < BLOCK_SIZE; k++) {
+            Csub += As[ty][k] * Bs[k][tx];
         }
 
         __syncthreads();
     }
 
     if (row < rowDimA && col < colDimB)
-        C[row * colDimB + col] = cValue;
+        C[row * colDimB + col] = Csub;
 }
 
 int main(int argc, char** argv) {
@@ -69,19 +94,22 @@ int main(int argc, char** argv) {
         h_B[i] = rand() / (float)RAND_MAX;
     }
 
+    
     // allocate device memory for matrices A, B, and C
     float *d_A, *d_B, *d_C;
     cudaMalloc((void**)&d_A, rowDimA * colDimA * sizeof(float));
     cudaMalloc((void**)&d_B, colDimA * colDimB * sizeof(float));
     cudaMalloc((void**)&d_C, rowDimA * colDimB * sizeof(float));
 
+    
     // copy host matrices A and B to device
     cudaMemcpy(d_A, h_A, rowDimA * colDimA * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, colDimA * colDimB * sizeof(float), cudaMemcpyHostToDevice);
 
-    // calculate grid and block dimensions
-    dim3 threadsPerBlock(TILE_SIZE, TILE_SIZE);
-    dim3 numBlocks((colDimB + TILE_SIZE - 1) / TILE_SIZE, (rowDimA + TILE_SIZE - 1) / TILE_SIZE);
+    
+    // initialize thread block and kernel grid dimensions 
+    dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 numBlocks((colDimB + BLOCK_SIZE - 1) / BLOCK_SIZE, (rowDimA + BLOCK_SIZE - 1) / BLOCK_SIZE);
 
     // invoke CUDA kernel
     matrixMul<<<numBlocks, threadsPerBlock>>>(d_A, d_B, d_C, rowDimA, colDimA, colDimB);
@@ -93,26 +121,23 @@ int main(int argc, char** argv) {
     printf("Matrix A:\n");
     for (int i = 0; i < rowDimA; i++) {
         for (int j = 0; j < colDimA; j++) {
-            printf("%.2f ", h_A[i * colDimA + j]);
+            printf("%.3f ", h_A[i * colDimA + j]);
         }
         printf("\n");
     }
 
-    // print results
     printf("Matrix B:\n");
     for (int i = 0; i < colDimA; i++) {
         for (int j = 0; j < colDimB; j++) {
-            printf("%.2f ", h_B[i * colDimB + j]);
+            printf("%.3f ", h_B[i * colDimB + j]);
         }
         printf("\n");
     }
     
-
-    // print results
     printf("Matrix C:\n");
     for (int i = 0; i < rowDimA; i++) {
         for (int j = 0; j < colDimB; j++) {
-            printf("%.2f ", h_C[i * colDimB + j]);
+            printf("%.3f ", h_C[i * colDimB + j]);
         }
         printf("\n");
     }
@@ -129,4 +154,3 @@ int main(int argc, char** argv) {
 
 return 0;
 }
-
